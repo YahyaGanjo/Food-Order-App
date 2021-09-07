cd6f47b2a268ed6808bfb61ce9ec9fddbebaef53
Yahya Ganjo <yahya.ganjo@gmail.com>
2021-09-07 14:42:09 +0200

    add items logic

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
+import CartContext from "../../store/cart-context";
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
 
 const HeaderCartButton = (props) => {
+  const ctx = useContext(CartContext);
+  const numberOfCartItems = ctx.items.reduce((acc, item) => {
+    return acc + item.amount;
+  }, 0);
   return (
     <button onClick={props.onOpen} className={classes.button}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
